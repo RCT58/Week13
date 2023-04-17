b2322ffd58cde7d04b2479524eec4a725c5931b3
RCT58 <122066661+RCT58@users.noreply.github.com>
2023-04-16 21:30:28 -0500

    Update package-info.java

--- jeep-sales/src/test/java/com/promineotech/jeep/controller/package-info.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/package-info.java
@@ -1,1 +1,22 @@
 package com.promineotech.jeep.controller;
+
+public class DefaultJeepSalesController {
+	@RestController
+	@RequestMapping("/jeeps")
+	public class DefaultJeepSalesController implements JeepSalesController {
+
+	  private final JeepSalesService jeepSalesService;
+
+	  public DefaultJeepSalesController(JeepSalesService jeepSalesService) {
+	    this.jeepSalesService = jeepSalesService;
+	  }
+
+	  @GetMapping
+	  @ResponseStatus(code = HttpStatus.OK)
+	  public List<Jeep> fetchJeeps(@RequestParam JeepModel model, 
+	                               @RequestParam String trim) {
+	    return jeepSalesService.fetchJeeps(model, trim);
+	  }
+	}
+
+}
